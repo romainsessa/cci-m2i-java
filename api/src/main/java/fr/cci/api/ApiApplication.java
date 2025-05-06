27ee3dc2a86997932e72cfcbac15c12f3a865f7e
romainsessa <romainsessa@gmail.com>
2025-05-06 16:18:56 +0200

    add role USER on db

--- api/src/main/java/fr/cci/api/ApiApplication.java
+++ api/src/main/java/fr/cci/api/ApiApplication.java
@@ -1,13 +1,32 @@
 package fr.cci.api;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import fr.cci.api.entities.ERole;
+import fr.cci.api.repositories.ERoleRepository;
+
 @SpringBootApplication
-public class ApiApplication {
+public class ApiApplication implements CommandLineRunner {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);		
+	}
+	
+	private ERoleRepository eRoleRepository;
+
+	public ApiApplication(ERoleRepository eRoleRepository) {
+		this.eRoleRepository = eRoleRepository;
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		ERole role = new ERole();
+		role.setName("USER");
+		eRoleRepository.save(role);	
+		
 	}
 
 }
